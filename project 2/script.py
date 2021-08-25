c49ec9c7e059f25d4641bd400c3329ac303b7167
Daniel <danielelielgaio@gmail.com>
2021-08-24 23:00:55 -0300

    Update script.py
    Adicionada a coleta dos títulos dos resultados e dos links.

--- project 2/script.py
+++ project 2/script.py
@@ -21,3 +21,24 @@ numero_resultados = int(
   
 maximo_paginas = numero_resultados/10
 print('Número de páginas: {}'.format(maximo_paginas))
+
+url_pagina = driver.find_element_by_xpath(
+  "//a[@aria-label='Page 2']"
+).get_attribute('href')
+
+pagina_atual = 0
+start = 10
+while pagina_atual < 10:
+  if not pagina_atual == 0:
+    url_pagina = url_pagina.replace('start=%s' % start, 'start=%s' % str(start+10))
+    start += 10
+  pagina_atual += 1
+  driver.get(url_pagina)
+  divs = driver.find_elements_by_xpath(
+    "//div[@class='g']"
+  )
+  for div in divs:
+    nome = div.find_element_by_tag_name('span')
+    link = div.find_element_by_tag_name('a')
+    resultado = '%s;%s' % (nome.text, link.get_attribute('href'))
+    print(resultado)
