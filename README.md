b957cb9b03fda93b95a605a95e6e9bf1ccb8b712
Daniel <danielelielgaio@gmail.com>
2021-12-04 09:44:40 -0300

    Escrita dos resultados coletados para um txt

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # python_rpa_studies
 
 Folder with projects and tests about RPA with python.
+
+Para rodar cada projeto deve ser criado um ambiente virtual (venv) separado.
+
+Ativar ambiente virtual: source env_p2/bin/activate
--- project 2/script.py
+++ project 2/script.py
@@ -20,7 +20,8 @@ numero_resultados = int(
   .replace('.', ''))
   
 maximo_paginas = numero_resultados/10
-print('Número de páginas: {}'.format(maximo_paginas))
+pagina_alvo = input(
+    '{} páginas encontradas, até qual página quer ir?'.format(maximo_paginas))
 
 url_pagina = driver.find_element_by_xpath(
   "//a[@aria-label='Page 2']"
@@ -28,10 +29,16 @@ url_pagina = driver.find_element_by_xpath(
 
 pagina_atual = 0
 start = 10
-while pagina_atual < 10:
-  if not pagina_atual == 0:
+lista_resultados = []
+
+while pagina_atual <= int(pagina_alvo)-1:
+  if pagina_atual > 1:
     url_pagina = url_pagina.replace('start=%s' % start, 'start=%s' % str(start+10))
     start += 10
+    driver.get(url_pagina)
+  elif pagina_atual == 1:
+    driver.get(url_pagina)
+    
   pagina_atual += 1
   driver.get(url_pagina)
   divs = driver.find_elements_by_xpath(
@@ -42,3 +49,10 @@ while pagina_atual < 10:
     link = div.find_element_by_tag_name('a')
     resultado = '%s;%s' % (nome.text, link.get_attribute('href'))
     print(resultado)
+    lista_resultados.append(resultado)
+
+with open('resultados.txt', 'w') as arquivo:
+  for resultado in lista_resultados:
+    arquivo.write('{}\n'.format(resultado))
+  arquivo.close()
+print('{} resultados encontrados do Google e salvo no arquivo resultados.txt'.format(len(lista_resultados)))
