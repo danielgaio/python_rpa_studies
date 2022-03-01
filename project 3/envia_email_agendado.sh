59e2898a37f5350b85edb6c5708fa8d7da53b954
Daniel <danielelielgaio@gmail.com>
2022-03-01 17:29:48 -0300

    Create envia_email_agendado.sh
    Trabalhando no shell script para disparo automático do envio de email

---
+++ project 3/envia_email_agendado.sh
@@ -0,0 +1,14 @@
+source '/home/daniel/Documents/GitHub/python_rpa_studies/project 3/env_p3/bin/activate'
+
+python3 '/home/daniel/Documents/GitHub/python_rpa_studies/project 3/envia_email_html_complexo.py'
+
+# Tornar .sh executavel
+# chmod +x 
+
+# adicionar jobs cron: crontab -e
+
+# cron job para rodar esse script
+# 37 22 * * * '~/Documents/GitHub/python_rpa_studies/project 3/envia_email_agendado.sh'
+# rodar a cada minuto
+# *  *    * * *   root    '/home/daniel/Documents/GitHub/python_rpa_studies/project 3/envia_email_agendado.sh'
+
